750015fe362e8c4463cdfb265ff49b831d18bdf5
David Ricaud <davidaricaud@gmail.com>
2021-07-01 12:19:22 -0500

    change dir to correct directory. If triggered by service, wd would be incorrect (looking at you GMMK Core)

--- toggleDevice.go
+++ toggleDevice.go
@@ -6,6 +6,8 @@ import (
 	"os/exec"
 	"os"
 	"io/ioutil"
+	"time"
+	"path/filepath"
 )
 
 //go:embed nircmd.exe
@@ -20,32 +22,57 @@ func unpackDeps() {
 		err := ioutil.WriteFile("nircmd.exe", nircmdBytes, 0770)
 		if err != nil {
 			fmt.Println("[ERROR] could not unpack nircmd.exe depencancy: ", err.Error())
+			time.Sleep(3*time.Second)
 			os.Exit(1)
 		}
 	}
 }
 
 func fileExists(path string) bool {
-	if _, err := os.Stat(path); err == nil {
-		return true 
-	} else {
-		return false  
-	}
+	 _, err := os.Stat(path); 
+	if err != nil {
+		return false 
+	} 
+	return true
 }
 
 func toggleDevices() {
 	var cmd *exec.Cmd
+
 	if fileExists("toggle.flag") {
-		cmd = exec.Command("nircmd.exe", "setdefaultsounddevice", "Headphones")
+		cmd = exec.Command("nircmd.exe", "setdefaultsounddevice", device1)
 		os.Remove("toggle.flag")
 	} else {
-		cmd = exec.Command("nircmd.exe", "setdefaultsounddevice", "ASUS PB277")
-		os.Create("toggle.flag")
+		cmd = exec.Command("nircmd.exe", "setdefaultsounddevice", device2)
+		_, err := os.Create("toggle.flag")
+		if err != nil {
+			printAndSleep(err.Error())
+		}
+		
 	}
 	cmd.Run()
 }
 
+func changeWD(){
+	wd, err := os.Executable()
+	if err != nil {
+		printAndSleep(err.Error())
+	}
+
+	err = os.Chdir(filepath.Dir(wd))
+	if err != nil {
+		printAndSleep(err.Error())
+	}
+
+}
+
 func main() {
+	changeWD()
+	unpackDeps()
 	toggleDevices()
 }
 
+func printAndSleep(msg string) {
+	fmt.Println(msg)
+	time.Sleep(3*time.Second)
+}
